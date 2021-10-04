c4977a10c916e93211346e10fbfc08474b4525d3
Joel Koreth <joelsymon@gmail.com>
2021-10-04 16:27:10 -0500

    Update Heart_Rate_Display_Arduino.ino

--- Software/Heart_Rate_Display_Arduino/Heart_Rate_Display_Arduino.ino
+++ Software/Heart_Rate_Display_Arduino/Heart_Rate_Display_Arduino.ino
@@ -26,22 +26,15 @@ Distributed as-is; no warranty is given.
 
 void setup() {
   // initialize the serial communication:
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(10, INPUT); // Setup for leads off detection LO +
   pinMode(11, INPUT); // Setup for leads off detection LO -
 
 }
 
 void loop() {
-  
-  if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
-    Serial.println('!');
-  }
-  else{
     // send the value of analog input 0:
-      Serial.println(analogRead(A0));
-  }
+      Serial.println(analogRead(A1));
   //Wait for a bit to keep serial data from saturating
-  delay(1);
+  delay(250);
 }
-
